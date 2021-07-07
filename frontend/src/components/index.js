886c5b51ada1cef1bb3533d7a502e4bc62375ada
sabbir hossain <sabbir.s.dk7@gmail.com>
2021-07-08 01:02:46 +0600

    clean the boilerplate and structured the app folder

---
+++ frontend/src/components/index.js
@@ -0,0 +1,7 @@
+import Blog from "./Blog";
+import BlogDetail from "./BlogDetail";
+import Category from "./Category";
+import Home from "./Home";
+import Navbar from "./Navbar";
+
+export { Blog, BlogDetail, Category, Home, Navbar };
